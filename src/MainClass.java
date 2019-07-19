d03fc67d40070ed0c3a41c0390de9e23b56d6f7e
Fanbir <borisenokpf@gmail.com>
2019-07-19 19:28:43 +0300

    delete space

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,11 +2,9 @@ import org.junit.Test;
 
 public class MainClass {
 
-
     private int class_number = 20;
 
     private String class_string = "Hello, world";
-
 
     public String getClassString() {
         return class_string;
